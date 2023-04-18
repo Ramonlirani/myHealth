4974860c295b4ad286923fb2895a52465b2b50d0
Ramonlirani <ramonlirani@hotmail.com>
2023-04-17 23:41:47 -0300

    feat: insert image card vaccine

--- app/components/Home/CardVaccine.jsx
+++ app/components/Home/CardVaccine.jsx
@@ -1,36 +1,46 @@
 import React from 'react'
-import { VStack, Box, Divider, Card, Text } from 'native-base'
+import { VStack, Box, Divider, Card, Text, Image, Center } from 'native-base'
 
 export const CardVaccine = () => {
   return (
-    <Box
-      border="1"
-      borderRadius="md"
-      width={'100%'}
-      alignItems={'center'}
-      flexDirection={'row'}
-    >
-      <Card
-        backgroundColor={'white'}
-        width={'50%'}
-        height={'40'}
-        marginLeft={2}
+    <Center>
+      <Box
+        border="1"
+        borderRadius="md"
+        width={'100%'}
+        alignItems={'center'}
+        flexDirection={'row'}
+        paddingTop={5}
       >
-        <Box alignItems={'center'}>
-          <Text color={'#355c7d'} bold>
-            Febre amarela
-          </Text>
-          <Box
-            backgroundColor={'#355c7d'}
-            borderRadius={6}
-            width={20}
-            alignItems={'center'}
-          >
-            <Text color={'white'}>1a. dose</Text>
+        <Card backgroundColor={'white'} width={'80%'} height={'56'}>
+          <Box alignItems={'center'}>
+            <Text color={'#355c7d'} bold>
+              Febre amarela
+            </Text>
+            <Box
+              backgroundColor={'#355c7d'}
+              borderRadius={6}
+              width={20}
+              alignItems={'center'}
+            >
+              <Text color={'white'}>1a. dose</Text>
+            </Box>
+            <Text>11/06/2022</Text>
+            <Image
+              marginTop={4}
+              width={200}
+              height={100}
+              source={require('../../../assets/images/vacina_febre_amarela.png')}
+              alt="Logo de Vacina"
+            />
           </Box>
-          <Text>11/06/2022</Text>
-        </Box>
-      </Card>
-    </Box>
+          <Box alignItems={'flex-end'}>
+            <Text color={'danger.500'} italic>
+              Próxima dose em: 11/10/2023
+            </Text>
+          </Box>
+        </Card>
+      </Box>
+    </Center>
   )
 }
--- app/screens/Home.jsx
+++ app/screens/Home.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { VStack } from 'native-base'
+import { ScrollView, VStack } from 'native-base'
 import { HeaderHome } from '../components/Home/HeaderHome'
 import { SearchBarHome } from '../components/Home/SearchBarHome'
 import { CardVaccine } from '../components/Home/CardVaccine'
@@ -8,7 +8,12 @@ export const Home = () => {
   return (
     <>
       <HeaderHome />
-      <VStack backgroundColor={'#24253c'} width={'100%'} height={'100%'}>
+      <VStack
+        backgroundColor={'#24253c'}
+        width={'100%'}
+        height={'100%'}
+        alignItems={'center'}
+      >
         <SearchBarHome />
         <CardVaccine />
       </VStack>
